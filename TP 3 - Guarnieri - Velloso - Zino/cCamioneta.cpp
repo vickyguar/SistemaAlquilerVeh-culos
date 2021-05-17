0c29fc027f78c9d66a5ef24681e46d69d7084bd2
Manu Velloso <manuvelloso2@gmail.com>
2021-05-16 21:03:22 -0300

    Constructor cCamioneta (y cmabio de nombre de la clase)

---
+++ TP 3 - Guarnieri - Velloso - Zino/cCamioneta.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////
+//  cCaminoeta.cpp
+//  Implementation of the Class cCaminoeta
+//  Created on:      14-may.-2021 15:26:51
+//  Original author: vguar
+///////////////////////////////////////////////////////////
+
+#include "cCamioneta.h"
+
+
+cCamioneta::cCamioneta(eEstadoVehiculo estado, float tarifa_base, eAdicionales adicionales, eColor color, unsigned int cant_alq, unsigned int capacidad, string chasis, string patente, string poliza, tm fecha_ult_mantenimiento):
+	cVehiculo::cVehiculo(estado, tarifa_base, adicionales, color, cant_alq, capacidad, chasis, patente, poliza, fecha_ult_mantenimiento)
+{
+}
+
+cCamioneta::~cCamioneta(){
+
+}
+
+void cCamioneta::AnadirAdicionales(){
+
+}
+
+
+float cCamioneta::CalcularTarifa(){
+
+	return 0;
+}
+
+
+void cCamioneta::PasosMantenimiento(){
+
+}
---
+++ TP 3 - Guarnieri - Velloso - Zino/cCamioneta.h
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////
+//  cCaminoeta.h
+//  Implementation of the Class cCaminoeta
+//  Created on:      14-may.-2021 15:26:51
+//  Original author: vguar
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_47399F2C_B863_4471_87EE_0238E7D8DCB0__INCLUDED_)
+#define EA_47399F2C_B863_4471_87EE_0238E7D8DCB0__INCLUDED_
+
+#include "cVehiculo.h"
+
+class cCamioneta : public cVehiculo
+{
+
+public:
+#pragma region CONSTRUCTORES & DESTRUCTORES
+
+	/**
+	* cCamioneta
+	* constructor de automovil
+	* @param estado del vehiculo, tarifa base, adicionales, color
+	* cantidad de alquileres, capacidad de pasajeros, chasis, patente, poliza, fecha de ultimo mantenimiento
+	*/
+	cCamioneta(eEstadoVehiculo estado, float tarifa_base, eAdicionales adicionales, eColor color, unsigned int cant_alq,
+		unsigned int capacidad, string chasis, string patente, string poliza, tm fecha_ult_mantenimiento);
+
+	/**
+	* ~cCamioneta
+	* destructor de la clase Camioneta
+	*/
+	virtual ~cCamioneta();
+
+#pragma endregion
+
+	void AnadirAdicionales();
+	float CalcularTarifa();
+	void PasosMantenimiento();
+
+};
+#endif // !defined(EA_47399F2C_B863_4471_87EE_0238E7D8DCB0__INCLUDED_)
